9fc5f6ca68fd761d40b76ca6bad5ffc01bb6ae05
vj00715 <110132141+vj00715@users.noreply.github.com>
2022-09-04 20:27:49 +0530

    Add files via upload

---
+++ MySQL/mySqlLinks.js
@@ -0,0 +1,8 @@
+const links = ["mysql_intro","mysql_TypeOfQuery"]
+const title = ["Intro","Type Of Query"]
+let totallinks = '<div class="topic">Topic</div>'
+for(i = 0; i < links.length;i++){
+    let linksTitle = '<a class="if_link" href="' + links[i] + '.html' + '" target="_parent">' + title[i]+ '</a>'
+
+    document.getElementsByClassName('sideBar')[0].innerHTML = totallinks = totallinks + linksTitle
+}
---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Navbar 
+const toggleButton = document.getElementsByClassName('toggle_btn')[0];
+const navbarLinks = document.getElementsByClassName('navbar_links')[0];
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active');
+});
+// 
+const toggleButton2 = document.getElementsByClassName('toggle_btn_2')[0];
+const navbarLinks2 = document.getElementsByClassName('sideBar')[0];
+
+toggleButton2.addEventListener('click', () => {
+    navbarLinks2.classList.toggle('active');
+});
